84a3e3658e5ffa2451b072b79c57cb6dde78c522
sohamwce <sohambpande79@gmail.com>
2025-11-11 12:21:06 +0530

    random

--- m.s
+++ m.s
@@ -1,4 +1,17 @@
 _start:
-    j _start
+    addi x1, x0, 2
+    addi x2, x0, 5
+    addi x3, x0, 0
+
+repeat:
+    add x3, x3, x1
+    addi x2, x2, -1
+    bne x2, x0, repeat
+
+
+j . 
+
+
+
 
     
